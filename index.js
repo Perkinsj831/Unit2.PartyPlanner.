7fd154d090e2fa3b708dc746268fbef19365319b
John Perkins <mrgreen@MrGreens-Laptop.local>
2024-02-14 18:18:47 -0600

    finished

---
+++ index.js
@@ -0,0 +1,79 @@
+const BASE_URL = 'https://fsa-crud-2aa9294fe819.herokuapp.com/api/room1'
+
+let events = []
+
+const eventsContainer = document.getElementById("events")
+
+const form = document.getElementById("addEvent")
+form.addEventListener("submit", addEvent)
+
+
+async function getEvents(){
+    try{
+        const response = await fetch(`${BASE_URL}/events`)
+        const json = await response.json()
+
+        return json.data
+    } catch(err) {
+        console.error(err)
+    }
+}
+
+async function addEvent(event) {
+    try {
+        event.preventDefault()
+
+        const id = form.id.value
+        const name = form.name.value
+        const date = form.date.value
+        const location = form.location.value
+        const description = form.description.value
+
+        const response = await fetch(`${BASE_URL}/events`, {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({
+                id: id,
+                name: name,
+                date: date,
+                location: location,
+                description: description
+            })
+        })
+        if (!response.ok) {
+            throw new Error("Error creating event!")
+        }
+
+        const json = await response.json()
+
+        render()
+    }   catch (error) {
+        console.error(error)
+    }
+}
+
+function renderEvents(){
+    const htmlEvents = events.map(event => {
+        let div = document.createElement("div")
+
+        div.className = "eventCard"
+
+        div.innerHTML = `<h3>#${event.id}</h3>
+                        <h4>${event.name}</h4>
+                        <h4>${event.date}</h4>
+                        <h4>${event.location}</h4>
+                        <p>${event.description}</p>`
+
+    return div
+    })
+
+    eventsContainer.replaceChildren(...htmlEvents)
+}
+
+async function startApp() {
+    events = await getEvents()
+
+    renderEvents()
+}
+
+startApp()
---
+++ newIndex.js
@@ -0,0 +1,82 @@
+const BASE_URL = "https://fsa-crud-2aa9294fe819.herokuapp.com/api/room1/events"
+
+let events = []
+
+const form = document.getElementById("addEvent")
+form.addEventListener("submit", addEvent)
+
+async function render() {
+    events = await getEvents()
+
+    const eventsElement = document.getElementById("events")
+
+    const eventCards = events.map((event) => {
+        const eventCard = document.createElement("div")
+        eventCard.classList.add("event")
+        eventCard.innerHTML = `
+            <h3>${event.id}</h3>
+            <h4>${event.name}</h4>
+            <h4>${event.date}</h4>
+            <h4>${event.location}</h4>
+            <p>${event.description}<p>
+        `
+
+        const deleteButton = document.createElement("button")
+        deleteButton.innerText = "Delete"
+        deleteButton.addEventListener("click", () => deleteEvent(event.id))
+        eventCard.append(deleteButton)
+        return eventCard
+    })
+
+    eventsElement.replaceChildren(...eventCards)
+}
+render()
+
+async function getEvents() {
+    try {
+        const response = await fetch(BASE_URL)
+        const json = await response.json()
+        return json.data
+    } catch (error){
+        console.error(error)
+    }
+}
+
+async function addEvent(event) {
+    try {
+        event.preventDefault()
+
+        const response = await fetch(BASE_URL, {
+            method: "POST",
+            headers: { "Content-Type": "application/json"},
+            body: JSON.stringify({
+                id: form.id.value,
+                name: form.name.value,
+                date: form.date.value,
+                location: form.location.value,
+                description: form.description.value
+            })
+        })
+        if (!response.ok) {
+            throw new Error("Error Creating Event!")
+        }
+
+        const json = await response.json()
+
+        render()
+    }   catch (error) {
+        console.error(error)
+    }
+}
+
+async function deleteEvent(eventId) {
+    try {
+        const response = await fetch(BASE_URL + "/" + eventId, {
+            method: "DELETE"
+        })
+
+        render()
+    } catch (error) {
+        console.error(error)
+    }
+}
